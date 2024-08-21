ebd1b4fdbdc3d883e4af502d4f7d1f2ca8f144b8
Hector <hector.martin@nextlink.com.tw>
2024-08-21 15:54:25 +0800

    commit 2

--- src/slapping/slap_that_like_button.py
+++ src/slapping/slap_that_like_button.py
@@ -1,4 +1,3 @@
-# I want to try github actions using pytest, pleae fill up this very document with simple code that I can use to test the github actions. 
 import pytest 
 
 def slap_that_like_button():
@@ -6,5 +5,3 @@ def slap_that_like_button():
 
 def test_slap_that_like_button():
     assert slap_that_like_button() == "Slapped that like button!"
-
-
